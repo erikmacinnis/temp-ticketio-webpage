352160f8934d3cc4c88cc3a6063b468cb8f465c5
erikmacinnis <erikhyun35@gmail.com>
2024-03-29 09:20:20 -0300

    feat: added privacy policy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,7 @@ const Footer = () => {
       <img src={ticketioLogo} alt='TicketIO'></img>
       <p>&copy; 2024 13213854 Canada Inc. All rights reserved.</p>
       <p>Contact: <a href="mailto:erikmacinnis@ticketio.ca">erikmacinnis@ticketio.ca</a></p>
+      <a href="/PrivacyPolicy.html" target="_blank" rel="noopener noreferrer">Privacy Policy</a>
     </footer>
  );
 };
